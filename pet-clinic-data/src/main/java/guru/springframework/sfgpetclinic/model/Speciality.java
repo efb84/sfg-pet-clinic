c88a6ae9c3d5f586a12669da2ca48f7d1617c20e
baykoc@gmail.com <baykoc@gmail.com>
2020-04-16 18:39:27 +0300

    added specialities. added to vet.Closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntitiy {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
