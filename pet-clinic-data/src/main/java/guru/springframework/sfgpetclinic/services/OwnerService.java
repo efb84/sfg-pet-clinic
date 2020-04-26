137b97178d135161ba87c0243bc09e8caf2247bc
baykoc@gmail.com <baykoc@gmail.com>
2020-04-26 22:45:58 +0300

    Added spring data jpa service for owners.Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import org.springframework.stereotype.Service;
-
-import java.util.Set;
 
 
 public interface OwnerService extends  CrudService<Owner,Long>{
